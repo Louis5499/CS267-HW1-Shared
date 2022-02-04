dfc5498a51f3a551d7f7317cdf290f540effb494
lloydbrownjr <lloydbrown@berkeley.edu>
2022-02-04 08:25:05 -0800

    working assembly

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -5,14 +5,156 @@
 
 const char* dgemm_desc = "Simple blocked dgemm.";
 
-#ifndef L1_BLOCK_SIZE
-#define L1_BLOCK_SIZE 128
+#ifndef BLOCK_SIZE
+#define BLOCK_SIZE 16
 #endif
-#ifndef L2_BLOCK_SIZE
-#define L2_BLOCK_SIZE 512
-#endif
-
 #define min(a, b) (((a) < (b)) ? (a) : (b))
+
+
+/*
+ * This auxiliary subroutine performs a smaller dgemm operation
+ *  C := C + A * B
+ * where C is M-by-N, A is M-by-K, and B is K-by-N.
+ */
+static inline __attribute__((optimize("unroll-loops"))) void asm_do_block(int lda, int M, int N, int K, double* A, double* B, double* C) {
+    __m512d cij, cij1, cij2, cij3, cij4, cij5, cij6, cij7;
+    __m512d ci8j, ci8j1, ci8j2, ci8j3, ci8j4, ci8j5, ci8j6, ci8j7;
+    __m512d aik, ai8k, bkj;
+    for (int i=0; i<M; i+=16) {
+        for (int j=0; j<N; j+=8) {
+            double* Cij = C + (i + j * lda);
+            asm volatile (
+                "vmovapd 0(%[Cij]), %[cij]\n\t"
+                "vmovapd 128(%[Cij]), %[cij1]\n\t"
+                "vmovapd 256(%[Cij]), %[cij2]\n\t"
+                "vmovapd 384(%[Cij]), %[cij3]\n\t"
+                "vmovapd 512(%[Cij]), %[cij4]\n\t"
+                "vmovapd 640(%[Cij]), %[cij5]\n\t"
+                "vmovapd 768(%[Cij]), %[cij6]\n\t"
+                "vmovapd 896(%[Cij]), %[cij7]\n\t"
+                : [cij] "+v" (cij),
+                [cij1] "+v" (cij1),
+                [cij2] "+v" (cij2),
+                [cij3] "+v" (cij3),
+                [cij4] "+v" (cij4),
+                [cij5] "+v" (cij5),
+                [cij6] "+v" (cij6),
+                [cij7] "+v" (cij7)
+                : [Cij] "r" (Cij)
+                : "memory"
+            );
+            asm volatile (
+                "vmovapd 64(%[Cij]), %[ci8j]\n\t"
+                "vmovapd 192(%[Cij]), %[ci8j1]\n\t"
+                "vmovapd 320(%[Cij]), %[ci8j2]\n\t"
+                "vmovapd 448(%[Cij]), %[ci8j3]\n\t"
+                "vmovapd 576(%[Cij]), %[ci8j4]\n\t"
+                "vmovapd 704(%[Cij]), %[ci8j5]\n\t"
+                "vmovapd 832(%[Cij]), %[ci8j6]\n\t"
+                "vmovapd 960(%[Cij]), %[ci8j7]\n\t"
+                : [ci8j] "+v" (ci8j),
+                [ci8j1] "+v" (ci8j1),
+                [ci8j2] "+v" (ci8j2),
+                [ci8j3] "+v" (ci8j3),
+                [ci8j4] "+v" (ci8j4),
+                [ci8j5] "+v" (ci8j5),
+                [ci8j6] "+v" (ci8j6),
+                [ci8j7] "+v" (ci8j7)
+                : [Cij] "r" (Cij)
+            );
+            for (int k=0; k<K; k+=1) {
+                double* Aik = A + (i + k * lda);
+                double* Ai8k = A + (i + 8 + k * lda);
+                double* Bkj = B + k + j * lda;
+                asm volatile (
+                    "vmovapd (%[Aik]), %[aik]\n\t"
+                    "vfmadd231pd 0(%[Bkj])%{1to8%}, %[aik], %[cij]\n\t"
+                    "vfmadd231pd 128(%[Bkj])%{1to8%}, %[aik], %[cij1]\n\t"
+                    "vfmadd231pd 256(%[Bkj])%{1to8%}, %[aik], %[cij2]\n\t"
+                    "vfmadd231pd 384(%[Bkj])%{1to8%}, %[aik], %[cij3]\n\t"
+                    "vfmadd231pd 512(%[Bkj])%{1to8%}, %[aik], %[cij4]\n\t"
+                    "vfmadd231pd 640(%[Bkj])%{1to8%}, %[aik], %[cij5]\n\t"
+                    "vfmadd231pd 768(%[Bkj])%{1to8%}, %[aik], %[cij6]\n\t"
+                    "vfmadd231pd 896(%[Bkj])%{1to8%}, %[aik], %[cij7]\n\t"
+                    : [aik] "+v" (aik),
+                    [cij] "+v" (cij),
+                    [cij6] "+v" (cij6),
+                    [cij5] "+v" (cij5),
+                    [cij4] "+v" (cij4),
+                    [cij2] "+v" (cij2),
+                    [cij3] "+v" (cij3),
+                    [cij7] "+v" (cij7),
+                    [cij1] "+v" (cij1)
+                    : [Aik] "r" (Aik),
+                    [Bkj] "r" (Bkj)
+                    : "memory"
+                );
+                asm volatile (
+                    "vmovapd (%[Ai8k]), %[ai8k]\n\t"
+                    "vfmadd231pd 0(%[Bkj])%{1to8%}, %[ai8k], %[ci8j]\n\t"
+                    "vfmadd231pd 128(%[Bkj])%{1to8%}, %[ai8k], %[ci8j1]\n\t"
+                    "vfmadd231pd 256(%[Bkj])%{1to8%}, %[ai8k], %[ci8j2]\n\t"
+                    "vfmadd231pd 384(%[Bkj])%{1to8%}, %[ai8k], %[ci8j3]\n\t"
+                    "vfmadd231pd 512(%[Bkj])%{1to8%}, %[ai8k], %[ci8j4]\n\t"
+                    "vfmadd231pd 640(%[Bkj])%{1to8%}, %[ai8k], %[ci8j5]\n\t"
+                    "vfmadd231pd 768(%[Bkj])%{1to8%}, %[ai8k], %[ci8j6]\n\t"
+                    "vfmadd231pd 896(%[Bkj])%{1to8%}, %[ai8k], %[ci8j7]\n\t"
+                    : [ci8j6] "+v" (ci8j6),
+                    [ci8j4] "+v" (ci8j4),
+                    [ci8j2] "+v" (ci8j2),
+                    [ci8j5] "+v" (ci8j5),
+                    [ci8j3] "+v" (ci8j3),
+                    [ci8j] "+v" (ci8j),
+                    [ci8j1] "+v" (ci8j1),
+                    [ci8j7] "+v" (ci8j7),
+                    [ai8k] "+v" (ai8k)
+                    : [Bkj] "r" (Bkj),
+                    [Ai8k] "r" (Ai8k)
+                );
+            }
+            asm volatile (
+                "vmovapd %[cij], 0(%[Cij])\n\t"
+                "vmovapd %[cij1], 128(%[Cij])\n\t"
+                "vmovapd %[cij2], 256(%[Cij])\n\t"
+                "vmovapd %[cij3], 384(%[Cij])\n\t"
+                "vmovapd %[cij4], 512(%[Cij])\n\t"
+                "vmovapd %[cij5], 640(%[Cij])\n\t"
+                "vmovapd %[cij6], 768(%[Cij])\n\t"
+                "vmovapd %[cij7], 896(%[Cij])\n\t"
+                : [cij3] "+v" (cij3),
+                [cij4] "+v" (cij4),
+                [cij6] "+v" (cij6),
+                [cij2] "+v" (cij2),
+                [cij5] "+v" (cij5),
+                [cij1] "+v" (cij1),
+                [cij] "+v" (cij),
+                [cij7] "+v" (cij7)
+                : [Cij] "r" (Cij)
+                : "memory"
+            );
+            asm volatile (
+                "vmovapd %[ci8j], 64(%[Cij])\n\t"
+                "vmovapd %[ci8j1], 192(%[Cij])\n\t"
+                "vmovapd %[ci8j2], 320(%[Cij])\n\t"
+                "vmovapd %[ci8j3], 448(%[Cij])\n\t"
+                "vmovapd %[ci8j4], 576(%[Cij])\n\t"
+                "vmovapd %[ci8j5], 704(%[Cij])\n\t"
+                "vmovapd %[ci8j6], 832(%[Cij])\n\t"
+                "vmovapd %[ci8j7], 960(%[Cij])\n\t"
+                : [ci8j3] "+v" (ci8j3),
+                [ci8j4] "+v" (ci8j4),
+                [ci8j6] "+v" (ci8j6),
+                [ci8j2] "+v" (ci8j2),
+                [ci8j5] "+v" (ci8j5),
+                [ci8j1] "+v" (ci8j1),
+                [ci8j] "+v" (ci8j),
+                [ci8j7] "+v" (ci8j7)
+                : [Cij] "r" (Cij)
+                : "memory"
+            );
+        }
+    }
+}
 
 /*
  * This auxiliary subroutine performs a smaller dgemm operation
@@ -25,191 +167,79 @@ static inline __attribute__((optimize("unroll-loops"))) void do_block(int lda, i
     __m512d aik, ai8k, bkj;
     for (int i=0; i<M; i+=16) {
         for (int j=0; j<N; j+=8) {
-            double* Cij = C + (i + j * lda);
-            double* Cij1 = C + (i + (j+1) * lda);
-            double* Cij2 = C + (i + (j+2) * lda);
-            double* Cij3 = C + (i + (j+3) * lda);
-            double* Cij4 = C + (i + (j+4) * lda);
-            double* Cij5 = C + (i + (j+5) * lda);
-            double* Cij6 = C + (i + (j+6) * lda);
-            double* Cij7 = C + (i + (j+7) * lda);
-            double* Ci8j = C + (i + 8 + j * lda);
-            double* Ci8j1 = C + (i + 8 + (j+1) * lda);
-            double* Ci8j2 = C + (i + 8 + (j+2) * lda);
-            double* Ci8j3 = C + (i + 8 + (j+3) * lda);
-            double* Ci8j4 = C + (i + 8 + (j+4) * lda);
-            double* Ci8j5 = C + (i + 8 + (j+5) * lda);
-            double* Ci8j6 = C + (i + 8 + (j+6) * lda);
-            double* Ci8j7 = C + (i + 8 + (j+7) * lda);
-            asm volatile (
-                "vmovapd (%[Cij]), %[cij]\n\t"
-                "vmovapd (%[Cij1]), %[cij1]\n\t"
-                "vmovapd (%[Cij2]), %[cij2]\n\t"
-                "vmovapd (%[Cij3]), %[cij3]\n\t"
-                "vmovapd (%[Cij4]), %[cij4]\n\t"
-                "vmovapd (%[Cij5]), %[cij5]\n\t"
-                "vmovapd (%[Cij6]), %[cij6]\n\t"
-                "vmovapd (%[Cij7]), %[cij7]\n\t"
-                : [cij] "+v" (cij),
-                [cij1] "+v" (cij1),
-                [cij2] "+v" (cij2),
-                [cij3] "+v" (cij3),
-                [cij4] "+v" (cij4),
-                [cij5] "+v" (cij5),
-                [cij6] "+v" (cij6),
-                [cij7] "+v" (cij7)
-                : [Cij] "r" (Cij),
-                [Cij1] "r" (Cij1),
-                [Cij2] "r" (Cij2),
-                [Cij3] "r" (Cij3),
-                [Cij4] "r" (Cij4),
-                [Cij5] "r" (Cij5),
-                [Cij6] "r" (Cij6),
-                [Cij7] "r" (Cij7)
-                : "memory"
-            );
-            asm volatile (
-                "vmovapd (%[Ci8j]), %[ci8j]\n\t"
-                "vmovapd (%[Ci8j1]), %[ci8j1]\n\t"
-                "vmovapd (%[Ci8j2]), %[ci8j2]\n\t"
-                "vmovapd (%[Ci8j3]), %[ci8j3]\n\t"
-                "vmovapd (%[Ci8j4]), %[ci8j4]\n\t"
-                "vmovapd (%[Ci8j5]), %[ci8j5]\n\t"
-                "vmovapd (%[Ci8j6]), %[ci8j6]\n\t"
-                "vmovapd (%[Ci8j7]), %[ci8j7]\n\t"
-                : [ci8j] "+v" (ci8j),
-                [ci8j1] "+v" (ci8j1),
-                [ci8j2] "+v" (ci8j2),
-                [ci8j3] "+v" (ci8j3),
-                [ci8j4] "+v" (ci8j4),
-                [ci8j5] "+v" (ci8j5),
-                [ci8j6] "+v" (ci8j6),
-                [ci8j7] "+v" (ci8j7)
-                : [Ci8j] "r" (Ci8j),
-                [Ci8j1] "r" (Ci8j1),
-                [Ci8j2] "r" (Ci8j2),
-                [Ci8j3] "r" (Ci8j3),
-                [Ci8j4] "r" (Ci8j4),
-                [Ci8j5] "r" (Ci8j5),
-                [Ci8j6] "r" (Ci8j6),
-                [Ci8j7] "r" (Ci8j7)
-                : "memory"
-            );
-            for (int k=0; k<K; k+=1) {
-                double* Aik = A + (i + k * lda);
-                double* Ai8k = A + (i + 8 + k * lda);
-                asm volatile (
-                    "vmovapd (%[Aik]), %[aik]\n\t"
-                    "vmovapd (%[Ai8k]), %[ai8k]\n\t"
-                    "vfmadd231pd %[bkj], %[aik], %([cij])"
-                    "vfmadd231pd %[bkj], %[ai8k], %([ci8j])"
-                    "vfmadd231pd %[bkj], %[aik], %([cij1])"
-                    "vfmadd231pd %[bkj], %[ai8k], %([ci8j1])"
-                    "vfmadd231pd %[bkj], %[aik], %([cij2])"
-                    "vfmadd231pd %[bkj], %[ai8k], %([ci8j2])"
-                    "vfmadd231pd %[bkj], %[aik], %([cij3])"
-                    "vfmadd231pd %[bkj], %[ai8k], %([ci8j3])"
-                    "vfmadd231pd %[bkj], %[aik], %([cij4])"
-                    "vfmadd231pd %[bkj], %[ai8k], %([ci8j4])"
-                    "vfmadd231pd %[bkj], %[aik], %([cij5])"
-                    "vfmadd231pd %[bkj], %[ai8k], %([ci8j5])"
-                    "vfmadd231pd %[bkj], %[aik], %([cij6])"
-                    "vfmadd231pd %[bkj], %[ai8k], %([ci8j6])"
-                    "vfmadd231pd %[bkj], %[aik], %([cij7])"
-                    "vfmadd231pd %[bkj], %[ai8k], %([ci8j7])"
-                    : [aik] "+v" (aik),
-                    [ai8k] "+v" (ai8k)
-                    : [Aik] "r" (Aik),
-                    [Ai8k] "r" (Ai8k),
-                    [ai8k] "r" (ai8k),
-                    [bkj] "r" (bkj),
-                    [ci8j] "r" (ci8j),
-                    [ci8j1] "r" (ci8j1),
-                    [ci8j2] "r" (ci8j2),
-                    [ci8j3] "r" (ci8j3),
-                    [ci8j4] "r" (ci8j4),
-                    [ci8j5] "r" (ci8j5),
-                    [ci8j6] "r" (ci8j6),
-                    [ci8j7] "r" (ci8j7)
-                    : "memory"
-                );
-            }
-            asm volatile (
-                "vmovapd (%[cij]), %[Cij]"
-                "vmovapd (%[cij1]), %[Cij1]"
-                "vmovapd (%[cij2]), %[Cij2]"
-                "vmovapd (%[cij3]), %[Cij3]"
-                "vmovapd (%[cij4]), %[Cij4]"
-                "vmovapd (%[cij5]), %[Cij5]"
-                "vmovapd (%[cij6]), %[Cij6]"
-                "vmovapd (%[cij7]), %[Cij7]"
-                : [cij] "+v" (cij),
-                [cij1] "+v" (cij1),
-                [cij2] "+v" (cij2),
-                [cij3] "+v" (cij3),
-                [cij4] "+v" (cij4),
-                [cij5] "+v" (cij5),
-                [cij6] "+v" (cij6),
-                [cij7] "+v" (cij7)
-                : [Cij] "r" (Cij),
-                [Cij1] "r" (Cij1),
-                [Cij2] "r" (Cij2),
-                [Cij3] "r" (Cij3),
-                [Cij4] "r" (Cij4),
-                [Cij5] "r" (Cij5),
-                [Cij6] "r" (Cij6),
-                [Cij7] "r" (Cij7)
-                : "memory"
-            );
-            asm volatile (
-                "vmovapd (%[ci8j]), %[Ci8j]"
-                "vmovapd (%[ci8j1]), %[Ci8j1]"
-                "vmovapd (%[ci8j2]), %[Ci8j2]"
-                "vmovapd (%[ci8j3]), %[Ci8j3]"
-                "vmovapd (%[ci8j4]), %[Ci8j4]"
-                "vmovapd (%[ci8j5]), %[Ci8j5]"
-                "vmovapd (%[ci8j6]), %[Ci8j6]"
-                "vmovapd (%[ci8j7]), %[Ci8j7]"
-                : [ci8j] "+v" (ci8j),
-                [ci8j1] "+v" (ci8j1),
-                [ci8j2] "+v" (ci8j2),
-                [ci8j3] "+v" (ci8j3),
-                [ci8j4] "+v" (ci8j4),
-                [ci8j5] "+v" (ci8j5),
-                [ci8j6] "+v" (ci8j6),
-                [ci8j7] "+v" (ci8j7)
-                : [Ci8j] "r" (Ci8j),
-                [Ci8j1] "r" (Ci8j1),
-                [Ci8j2] "r" (Ci8j2),
-                [Ci8j3] "r" (Ci8j3),
-                [Ci8j4] "r" (Ci8j4),
-                [Ci8j5] "r" (Ci8j5),
-                [Ci8j6] "r" (Ci8j6),
-                [Ci8j7] "r" (Ci8j7)
-                : "memory"
-            );
-        }
-    }
-}
+            cij = _mm512_loadu_pd(C + (i + j * lda));
+            cij1 = _mm512_loadu_pd(C + (i + (j+1) * lda));
+            cij2 = _mm512_loadu_pd(C + (i + (j+2) * lda));
+            cij3 = _mm512_loadu_pd(C + (i + (j+3) * lda));
+            cij4 = _mm512_loadu_pd(C + (i + (j+4) * lda));
+            cij5 = _mm512_loadu_pd(C + (i + (j+5) * lda));
+            cij6 = _mm512_loadu_pd(C + (i + (j+6) * lda));
+            cij7 = _mm512_loadu_pd(C + (i + (j+7) * lda));
 
-/* This routine performs a dgemm operation
- *  C := C + A * B
- * where A, B, and C are lda-by-lda matrices stored in column-major format.
- * On exit, A and B maintain their input values. */
-void l2_dgemm(int lda, int M, int N, int K, double* A, double* B, double* C) {
-    // For each block-row of A
-    for (int j = 0; j < N; j += L1_BLOCK_SIZE) {
-        for (int k = 0; k < K; k += L1_BLOCK_SIZE) {
-            for (int i = 0; i < M; i += L1_BLOCK_SIZE) {
-            // For each block-column of B
-                // Accumulate block dgemms into block of C
-                // Correct block dimensions if block "goes off edge of" the matrix
-                int L1_M = min(L1_BLOCK_SIZE, M - i);
-                int L1_N = min(L1_BLOCK_SIZE, N - j);
-                int L1_K = min(L1_BLOCK_SIZE, K - k);
-                // Perform individual block dgemm
-                do_block(lda, L1_M, L1_N, L1_K, A + i + k * lda, B + k + j * lda, C + i + j * lda);
+            ci8j = _mm512_loadu_pd(C + (i + 8 + j * lda));
+            ci8j1 = _mm512_loadu_pd(C + (i + 8 + (j+1) * lda));
+            ci8j2 = _mm512_loadu_pd(C + (i + 8 + (j+2) * lda));
+            ci8j3 = _mm512_loadu_pd(C + (i + 8 + (j+3) * lda));
+            ci8j4 = _mm512_loadu_pd(C + (i + 8 + (j+4) * lda));
+            ci8j5 = _mm512_loadu_pd(C + (i + 8 + (j+5) * lda));
+            ci8j6 = _mm512_loadu_pd(C + (i + 8 + (j+6) * lda));
+            ci8j7 = _mm512_loadu_pd(C + (i + 8 + (j+7) * lda));
+
+            for (int k=0; k<K; k+=1) {
+                aik = _mm512_loadu_pd(A + (i + k * lda));
+                ai8k = _mm512_loadu_pd(A + (i + 8 + k * lda));
+
+                bkj = _mm512_set1_pd(B[k + j * lda]);
+                cij = _mm512_fmadd_pd(aik, bkj, cij);
+                ci8j = _mm512_fmadd_pd(ai8k, bkj, ci8j);
+
+                bkj = _mm512_set1_pd(B[k + (j+1) * lda]);
+                cij1 = _mm512_fmadd_pd(aik, bkj, cij1);
+                ci8j1 = _mm512_fmadd_pd(ai8k, bkj, ci8j1);
+
+                bkj = _mm512_set1_pd(B[k + (j+2) * lda]);
+                cij2 = _mm512_fmadd_pd(aik, bkj, cij2);
+                ci8j2 = _mm512_fmadd_pd(ai8k, bkj, ci8j2);
+
+                bkj = _mm512_set1_pd(B[k + (j+3) * lda]);
+                cij3 = _mm512_fmadd_pd(aik, bkj, cij3);
+                ci8j3 = _mm512_fmadd_pd(ai8k, bkj, ci8j3);
+
+                bkj = _mm512_set1_pd(B[k + (j+4) * lda]);
+                cij4 = _mm512_fmadd_pd(aik, bkj, cij4);
+                ci8j4 = _mm512_fmadd_pd(ai8k, bkj, ci8j4);
+
+                bkj = _mm512_set1_pd(B[k + (j+5) * lda]);
+                cij5 = _mm512_fmadd_pd(aik, bkj, cij5);
+                ci8j5 = _mm512_fmadd_pd(ai8k, bkj, ci8j5);
+
+                bkj = _mm512_set1_pd(B[k + (j+6) * lda]);
+                cij6 = _mm512_fmadd_pd(aik, bkj, cij6);
+                ci8j6 = _mm512_fmadd_pd(ai8k, bkj, ci8j6);
+
+                bkj = _mm512_set1_pd(B[k + (j+7) * lda]);
+                cij7 = _mm512_fmadd_pd(aik, bkj, cij7);
+                ci8j7 = _mm512_fmadd_pd(ai8k, bkj, ci8j7);
+
             }
+            _mm512_store_pd(C + i + j * lda, cij);
+            _mm512_store_pd(C + i + (j+1) * lda, cij1);
+            _mm512_store_pd(C + i + (j+2) * lda, cij2);
+            _mm512_store_pd(C + i + (j+3) * lda, cij3);
+            _mm512_store_pd(C + i + (j+4) * lda, cij4);
+            _mm512_store_pd(C + i + (j+5) * lda, cij5);
+            _mm512_store_pd(C + i + (j+6) * lda, cij6);
+            _mm512_store_pd(C + i + (j+7) * lda, cij7);
+
+
+            _mm512_store_pd(C + i + 8 + j * lda, ci8j);
+            _mm512_store_pd(C + i + 8 + (j+1) * lda, ci8j1);
+            _mm512_store_pd(C + i + 8 + (j+2) * lda, ci8j2);
+            _mm512_store_pd(C + i + 8 + (j+3) * lda, ci8j3);
+            _mm512_store_pd(C + i + 8 + (j+4) * lda, ci8j4);
+            _mm512_store_pd(C + i + 8 + (j+5) * lda, ci8j5);
+            _mm512_store_pd(C + i + 8 + (j+6) * lda, ci8j6);
+            _mm512_store_pd(C + i + 8 + (j+7) * lda, ci8j7);
         }
     }
 }
@@ -240,17 +270,17 @@ void square_dgemm(int lda, double* A, double* B, double* C) {
     }
 
     // For each block-row of A
-    for (int j = 0; j < n; j += L2_BLOCK_SIZE) {
-        for (int k = 0; k < n; k += L2_BLOCK_SIZE) {
-            for (int i = 0; i < n; i += L2_BLOCK_SIZE) {
+    for (int j = 0; j < n; j += BLOCK_SIZE) {
+        for (int k = 0; k < n; k += BLOCK_SIZE) {
+            for (int i = 0; i < n; i += BLOCK_SIZE) {
             // For each block-column of B
                 // Accumulate block dgemms into block of C
                 // Correct block dimensions if block "goes off edge of" the matrix
-                int M = min(L2_BLOCK_SIZE, n - i);
-                int N = min(L2_BLOCK_SIZE, n - j);
-                int K = min(L2_BLOCK_SIZE, n - k);
+                int M = min(BLOCK_SIZE, n - i);
+                int N = min(BLOCK_SIZE, n - j);
+                int K = min(BLOCK_SIZE, n - k);
                 // Perform individual block dgemm
-                l2_dgemm(n, M, N, K, A_trans + i + k * n, B_cpy + k + j * n, C_cpy + i + j * n);
+                asm_do_block(n, M, N, K, A_trans + i + k * n, B_cpy + k + j * n, C_cpy + i + j * n);
             }
         }
     }
